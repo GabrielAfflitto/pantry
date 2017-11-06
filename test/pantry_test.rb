60e83f01c0ad2775ec9540f1c67dda6f8ab2409e
Gabe Afflitto <gafflitto92@gmail.com>
2017-11-06 13:13:22 -0700

    changed hash from empty hash to default value 0

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -3,5 +3,21 @@ require 'minitest/autorun'
 require 'minitest/pride'
 
 class PantryTest < Minitest::Test
+  def test_it_exists
+    pantry = Pantry.new
 
+    assert_instance_of Pantry, pantry
+  end
+
+  def test_stock_defaults_to_empty_array
+    pantry = Pantry.new
+
+    assert_equal ({}), pantry.stock
+  end
+
+  def test_that_stock_check_looks_for_given_item
+    pantry = Pantry.new
+
+    assert_equal 0, pantry.stock_check("cheese")
+  end
 end
