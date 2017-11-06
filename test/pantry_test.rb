b64cad404b6ecf5f0c90547f9e2b5d8a89fcfb49
Gabe Afflitto <gafflitto92@gmail.com>
2017-11-06 15:15:48 -0700

    building out iteration 3 tests

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -91,4 +91,46 @@ class PantryTest < Minitest::Test
 
     assert_equal list, pantry.print_shopping_list
   end
+
+  def test_what_can_I_make
+    pantry = Pantry.new
+    pantry.add_to_cookbook(recipe1)
+    pantry.add_to_cookbook(recipe2)
+    pantry.add_to_cookbook(recipe3)
+    pantry_filler
+
+    assert_equal ["Pickles", "Peanuts"], pantry.what_can_i_make
+  end
+
+  def test_case_name
+
+  end
+
+  def pantry_filler
+    pantry.restock("Cheese", 10)
+    pantry.restock("Flour", 20)
+    pantry.restock("Brine", 40)
+    pantry.restock("Raw nuts", 20)
+    pantry.restock("Salt", 20)
+  end
+
+  def recipe1
+    r1 = Recipe.new("Cheese Pizza")
+    r1.add_ingredient("Cheese", 20)
+    r1.add_ingredient("Flour", 20)
+  end
+
+  def recipe2
+    r2 = Recipe.new("Pickles")
+    r2.add_ingredient("Brine", 10)
+    r2.add_ingredient("Cucumbers", 30)
+  end
+
+  def recipe3
+    r3 = Recipe.new("Peanuts")
+    r3.add_ingredient("Raw nuts", 10)
+    r3.add_ingredient("Salt", 10)
+  end
+
+
 end
